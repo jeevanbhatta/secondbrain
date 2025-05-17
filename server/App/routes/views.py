1d6f6e4d74084b852b103961e755bf07ff2c25c3
Sana Mehta <106241946+sanamehta@users.noreply.github.com>
2025-05-17 15:39:08 -0700

    Group by date

--- server/App/routes/views.py
+++ server/App/routes/views.py
@@ -1,12 +1,15 @@
 from flask import render_template, jsonify
 from ..models import SavedPage
 from . import main_bp
+from datetime import datetime, timedelta
 
 
 @main_bp.route('/')
 def index():
     saved_pages = SavedPage.query.order_by(SavedPage.saved_at.desc()).all()
-    return render_template('home.html', saved_pages=saved_pages)
+    today = datetime.now().strftime('%Y-%m-%d')
+    yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+    return render_template('home.html', saved_pages=saved_pages, today=today, yesterday=yesterday)
 
 
 @main_bp.route('/about')
